69ce8a0575e349cb59ba360db703c48ad5f9940a
Evan Tu <zhtubo@163.com>
2014-10-28 15:14:56 +0800

    modify category

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,12 @@ if ($(window).width() <= 1280) {
 
 // Variables
     tag1       = $('.pl__all'),
-    tag2       = $('.place_your_tag_name_here'),
-    tag3       = $('.place_your_tag_name_here'),
-    tag4       = $('.place_your_tag_name_here'),
-    tag5       = $('.place_your_tag_name_here'),
-    tag6       = $('.place_your_tag_name_here');
+    tag2       = $('.ios'),
+    tag3       = $('.nodejs'),
+    tag4       = $('.database'),
+    tag5       = $('.net'),
+    tag6       = $('.lift');
+    tag7       = $('.other');
 var sidebar    = $('#sidebar'),
     container  = $('#post'),
     content    = $('#pjax'),
@@ -30,7 +31,7 @@ var clickHandler = function(k) {
     window['tag'+k].delay(50).fadeIn(350);
   }
 };
-for (var i = 1; i <= 6; i++) {
+for (var i = 1; i <= 7; i++) {
   $('#js-label' + i).on('click', clickHandler(i));
 }
 
